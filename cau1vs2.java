502d1b99cc4f58bc8e4c058bfee325657adf1333
Aransu <92172284+Aransu@users.noreply.github.com>
2023-03-16 15:00:41 +0700

    Add files via upload

---
+++ cau1vs2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class cau1vs2 {
+    static int cau1(int x,int y){
+            Scanner scanin= new Scanner(System.in);
+            int result=0;
+            if(x==0||y==0)
+                result=x+y;
+            else{
+                while(x!=y){
+                    if(x>y)
+                    x-=y;
+                    else
+                    y-=x;
+                }
+                result=x;
+            }
+            System.out.printf("Uoc so chung lon nhat cua 2 so vua nhap la: " + result);
+            return result;
+    }
+    public static void main(String[] args){
+        int x,y;
+        Scanner scanin= new Scanner(System.in);
+        x=scanin.nextInt();
+        y=scanin.nextInt();
+        int result2=0;
+        result2=x*y/cau1(x,y);
+        System.out.printf("\nBoi so chung nho nhat cua 2 so vua nhap la: " + result2);
+    }
+}
