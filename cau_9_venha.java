33bb8d9f75b804e562f60989c3f72fc41c37ec99
sang <sangfc774@gmail.com>
2023-03-16 17:32:24 +0700

    delete file

--- cau_9_venha.java
+++ cau_9_venha.java
@@ -9,13 +9,10 @@ public class cau_9_venha {
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
 
-            // Remove non-alphanumeric characters
             String cleanLine = line.replaceAll("[^A-Za-z0-9]", "");
 
-            // Reverse the string
             String reverseLine = new StringBuilder(cleanLine).reverse().toString();
 
-            // Check if the original and reversed lines are equal
             if (cleanLine.equalsIgnoreCase(reverseLine)) {
                 System.out.println(line + " is a palindrome.");
             } else {
