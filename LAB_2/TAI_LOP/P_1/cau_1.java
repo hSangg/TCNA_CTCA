513c5237db1a344d9fa14bbe04af42bda25e7d6b
sang <sangfc774@gmail.com>
2023-03-31 17:56:27 +0700

    Hoai Sang completed all tasks

--- LAB_2/TAI_LOP/P_1/cau_1.java
+++ LAB_2/TAI_LOP/P_1/cau_1.java
@@ -1,5 +1,28 @@
 package LAB_2.TAI_LOP.P_1;
 
-public class cau_1 {
+class Point {
+    public int x;
+    public int y;
 
+    Point() {
+        this.x = this.y = 0;
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point x) {
+        return Math.sqrt(Math.pow((this.x - x.x), 2) + Math.pow((this.y - x.y), 2));
+    }
+}
+
+public class cau_1 {
+    public static void main(String[] args) {
+        Point x = new Point(0, 2);
+        Point y = new Point(0, 4);
+
+        System.out.println(x.distance(y));
+    }
 }
